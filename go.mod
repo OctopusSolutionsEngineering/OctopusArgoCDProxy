30e487cc33506bbc27c92ed0eb0cfd0b53e78003
Matthew Casperson <matthewcasperson@gmail.com>
2023-08-03 14:32:44 +1000

    Added caching of Octopus API calls

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/OctopusDeploy/go-octopusdeploy v1.8.6
+	github.com/allegro/bigcache/v3 v3.1.0
 	github.com/argoproj/argo-cd/v2 v2.7.10
 	github.com/gin-gonic/gin v1.9.1
 	github.com/samber/lo v1.38.1
--- internal/infrastructure/octopus/deploy_release.go
+++ internal/infrastructure/octopus/deploy_release.go
@@ -1,16 +1,20 @@
 package octopus
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
 	"github.com/OctopusSolutionsEngineering/OctopusArgoCDProxy/internal/domain/models"
 	"github.com/OctopusSolutionsEngineering/OctopusArgoCDProxy/internal/domain/versioning"
 	"github.com/OctopusSolutionsEngineering/OctopusArgoCDProxy/internal/infrastructure/logging"
+	"github.com/allegro/bigcache/v3"
 	"github.com/samber/lo"
 	"net/url"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 const MaxInt = 2147483647
@@ -22,6 +26,7 @@ type LiveOctopusClient struct {
 	client    *octopusdeploy.Client
 	logger    logging.AppLogger
 	versioner versioning.ReleaseVersioner
+	bigCache  *bigcache.BigCache
 }
 
 func NewLiveOctopusClient(versioner versioning.ReleaseVersioner) (*LiveOctopusClient, error) {
@@ -37,10 +42,13 @@ func NewLiveOctopusClient(versioner versioning.ReleaseVersioner) (*LiveOctopusCl
 		return nil, err
 	}
 
+	bCache, err := bigcache.New(context.Background(), bigcache.DefaultConfig(5*time.Minute))
+
 	return &LiveOctopusClient{
 		client:    client,
 		logger:    logger,
 		versioner: versioner,
+		bigCache:  bCache,
 	}, nil
 }
 
@@ -109,14 +117,15 @@ func getClient() (*octopusdeploy.Client, error) {
 
 // getProject scans Octopus for the project that has been linked to the Argo CD Application and namespace
 func (o *LiveOctopusClient) getProject(application string, namespace string) ([]models.ArgoCDProject, error) {
-	projects, err := o.client.Projects.Get(octopusdeploy.ProjectsQuery{Take: MaxInt})
+
+	projects, err := o.getAllProject()
 
 	if err != nil {
 		return nil, err
 	}
 
 	matchingProjects := lo.FilterMap(projects.Items, func(project *octopusdeploy.Project, index int) (models.ArgoCDProject, bool) {
-		variables, err := o.client.Variables.GetAll(project.ID)
+		variables, err := o.getProjectVariables(project.ID)
 
 		if err != nil {
 			return models.ArgoCDProject{}, false
@@ -161,51 +170,153 @@ func (o *LiveOctopusClient) getProject(application string, namespace string) ([]
 	return matchingProjects, nil
 }
 
-func (o *LiveOctopusClient) getDefaultChannel(project *octopusdeploy.Project) (*octopusdeploy.Channel, error) {
-	channelQuery := octopusdeploy.ChannelsQuery{
-		Take: MaxInt,
-		Skip: 0,
+func (o *LiveOctopusClient) getProjectVariables(projectId string) (*octopusdeploy.VariableSet, error) {
+	// Load variables, and cache the results
+	variables := &octopusdeploy.VariableSet{}
+	variablesData, err := o.bigCache.Get(projectId + "-Variables")
+	if err == nil {
+		err = json.Unmarshal(variablesData, variables)
+
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		freshVariables, err := o.client.Variables.GetAll(projectId)
+		variables = &freshVariables
+
+		if err != nil {
+			return nil, err
+		}
+
+		variablesData, err = json.Marshal(freshVariables)
+
+		if err != nil {
+			return nil, err
+		}
+
+		o.bigCache.Set(projectId+"-Variables", variablesData)
 	}
 
-	channels, err := o.client.Channels.Get(channelQuery)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defaultChannel := lo.Filter(channels.Items, func(item *octopusdeploy.Channel, index int) bool {
-		return item.IsDefault && item.ProjectID == project.ID
-	})
-
-	if len(defaultChannel) == 1 {
-		return defaultChannel[0], nil
-	}
-
-	return nil, errors.New("could not find the default channel")
+	return variables, nil
 }
 
-func (o *LiveOctopusClient) getEnvironmentId(environment string) (string, error) {
-	if strings.HasPrefix("Environments-", environment) {
-		return environment, nil
+func (o *LiveOctopusClient) getAllProject() (*octopusdeploy.Projects, error) {
+	// Load projects, and cache the results
+	projects := &octopusdeploy.Projects{}
+	projectsData, err := o.bigCache.Get("AllProjects")
+	if err == nil {
+		err = json.Unmarshal(projectsData, projects)
+
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		projects, err = o.client.Projects.Get(octopusdeploy.ProjectsQuery{Take: MaxInt})
+
+		if err != nil {
+			return nil, err
+		}
+
+		projectsData, err = json.Marshal(projects)
+
+		if err != nil {
+			return nil, err
+		}
+
+		o.bigCache.Set("AllProjects", projectsData)
 	}
 
-	environmentsQuery := octopusdeploy.EnvironmentsQuery{
-		Name: environment,
+	return projects, nil
+}
+
+func (o *LiveOctopusClient) getDefaultChannel(project *octopusdeploy.Project) (*octopusdeploy.Channel, error) {
+
+	// Load variables, and cache the results
+	channel := &octopusdeploy.Channel{}
+	channelData, err := o.bigCache.Get(project.ID + "-DefaultChannel")
+	if err == nil {
+		err = json.Unmarshal(channelData, channel)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return channel, nil
+	} else {
+		channelQuery := octopusdeploy.ChannelsQuery{
+			Take: MaxInt,
+			Skip: 0,
+		}
+
+		channels, err := o.client.Channels.Get(channelQuery)
+
+		if err != nil {
+			return nil, err
+		}
+
+		defaultChannel := lo.Filter(channels.Items, func(item *octopusdeploy.Channel, index int) bool {
+			return item.IsDefault && item.ProjectID == project.ID
+		})
+
+		if len(defaultChannel) != 1 {
+			return nil, errors.New("could not find the default channel")
+		}
+
+		channelData, err = json.Marshal(defaultChannel[0])
+
+		if err != nil {
+			return nil, err
+		}
+
+		o.bigCache.Set(project.ID+"-DefaultChannel", channelData)
+
+		return defaultChannel[0], nil
+	}
+}
+
+func (o *LiveOctopusClient) getEnvironmentId(environmentName string) (string, error) {
+	if strings.HasPrefix("Environments-", environmentName) {
+		return environmentName, nil
 	}
 
-	environments, err := o.client.Environments.Get(environmentsQuery)
+	// Load environments, and cache the results
+	environment := &octopusdeploy.Environment{}
+	environmentData, err := o.bigCache.Get("Environments-" + environmentName)
+	if err == nil {
+		err = json.Unmarshal(environmentData, environment)
 
-	if err != nil {
-		return "", nil
-	}
+		if err != nil {
+			return "", err
+		}
 
-	filteredEnvironments := lo.Filter(environments.Items, func(e *octopusdeploy.Environment, index int) bool {
-		return e.Name == environment
-	})
+		return environment.ID, nil
+	} else {
+		environmentsQuery := octopusdeploy.EnvironmentsQuery{
+			Name: environmentName,
+		}
 
-	if len(filteredEnvironments) == 1 {
+		environments, err := o.client.Environments.Get(environmentsQuery)
+
+		if err != nil {
+			return "", nil
+		}
+
+		filteredEnvironments := lo.Filter(environments.Items, func(e *octopusdeploy.Environment, index int) bool {
+			return e.Name == environmentName
+		})
+
+		if len(filteredEnvironments) != 1 {
+			return "", errors.New("failed to find an environment called " + environmentName)
+		}
+
+		environmentData, err = json.Marshal(filteredEnvironments[0])
+
+		if err != nil {
+			return "", err
+		}
+
+		o.bigCache.Set("Environments-"+environmentName, environmentData)
+
 		return filteredEnvironments[0].ID, nil
 	}
-
-	return "", errors.New("failed to find an environment called " + environment)
 }
