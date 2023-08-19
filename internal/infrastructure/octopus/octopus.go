fe9243c908f8974391661130f71939ed1725a0a4
Matthew Casperson <matthewcasperson@gmail.com>
2023-08-19 13:07:41 +1000

    Added more comments

--- internal/infrastructure/octopus/octopus.go
+++ internal/infrastructure/octopus/octopus.go
@@ -7,6 +7,8 @@ import (
 type OctopusClient interface {
 	// CreateAndDeployRelease will ensure the release is deployed to the correct environment, creating a new release if necessary
 	CreateAndDeployRelease(updateMessage models.ApplicationUpdateMessage) error
+	// GetReleaseVersions returns the releases associated with a project
 	GetReleaseVersions(projectId string) ([]string, error)
+	// IsDeployed returns true if the release is deployed to the specified environment
 	IsDeployed(projectId string, releaseVersion string, environmentName string) (bool, error)
 }
